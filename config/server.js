4c487d65dd1c6c2a7d201d823d8a72b4c73312dd
Alexander Hansen <Alexander-hansen@hotmail.com>
2021-12-08 11:26:22 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a42a89c0dc519731c3bda79f6affce72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '669131b5-e34b-4f4b-bd35-1f1b47711e42'
+};
